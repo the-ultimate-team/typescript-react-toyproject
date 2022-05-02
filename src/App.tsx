1f7813c9a4b20437b05270f7dcbea66d8ac58630
eunhasublue <eunhasublooming@gmail.com>
2022-05-02 20:14:10 +0900

    feat: RecoilRoot

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,26 @@ import NavigaionBar from "./components/NavigaionBar";
 import styled from "styled-components";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { routePages } from "./routes/index";
+import { RecoilRoot } from "recoil";
 
 const App: React.FC = () => {
   return (
-    <AppStyle>
-      <BrowserRouter>
-        <Routes>
-          {routePages.map((route, i) => (
-            <Route
-              key={`routes__${i}`}
-              path={route.path}
-              element={route.element}
-            />
-          ))}
-        </Routes>
-        <NavigaionBar />
-      </BrowserRouter>
-    </AppStyle>
+    <RecoilRoot>
+      <AppStyle>
+        <BrowserRouter>
+          <Routes>
+            {routePages.map((route, i) => (
+              <Route
+                key={`routes__${i}`}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+          </Routes>
+          <NavigaionBar />
+        </BrowserRouter>
+      </AppStyle>
+    </RecoilRoot>
   );
 };
 
